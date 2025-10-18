40ba7bf6da19667c53c1d778aa0d4064f23d60c8
lmaoaoaoao <jafar.radzhaboff@yandex.ru>
2025-10-18 21:15:51 +0300

    камень-ножницы-бумага

--- P Works/P2/game.py
+++ P Works/P2/game.py
@@ -1,52 +1,55 @@
 from tkinter import *
+from random import *
 
 #создание и настройка главного окна
 window = Tk() #создание окна
 
-window.title('game v0.1') #имя окна
-window.iconbitmap('gameLogo.ico') #иконка окна
+window.title('game v0.2') #имя окна
 
-window.geometry('1200x600') #размер
+window.geometry('600x400') #размер
 window.resizable(0,0) #запрет на изменение размера окна
 
 window.config(bg = 'lime') #цвет фона
 
+#Функции
+def sps():
+    sps = ['Stone', 'Paper', 'Scissors']
+    value = choice(sps)
+    label_Text.configure (text=value)
+
+
+
+
+
 
 #Виджеты
 
-close_Button = Button(window,
-                      width=20,
-                      height=2,
-                      text='quit',
-                      command=quit,
-                      font = ('Comic Sans MS', 20, 'italic'),
-                      bg = 'purple',
-                      activebackground='indigo',
-                      fg='black',
-                      activeforeground="green"
-                      )
-close_Button.pack()
-close_Button.place(x = 850, y = 10)
+label_Text = Label(window, text='', fg ='white', font=('Comic Snas MS', 20), bg = "black")
+label_Text.place(y=150, x=250)
 
-label = Label(window,
-                      width=20,
-                      height=2,
-                      text='quit',
-                      font = ('Comic Sans MS', 20, 'italic'),
-                      bg = 'purple',
-                      fg='black'
-                      )
-label.pack()
-label.place(x = 100, y = 10)
+stone = Button(window,
+               text='Stone',
+               font=('Comic Snas MS', 20), 
+               bg = "white",
+               command=sps
+               )
+stone.place(x=50, y=300)
 
+paper = Button(window,
+               text='Paper',
+               font=('Comic Snas MS', 20), 
+               bg = "white",
+               command=sps
+               )
+paper.place(x=250, y=300)
 
-
-img = PhotoImage(file='gameLogo.png')
-l_logo = Label(window, image=img)
-l_logo.pack()
-
-
-
+scissors = Button(window,
+               text='Scissors',
+               font=('Comic Snas MS', 20), 
+               bg = "white",
+               command=sps
+               )
+scissors.place(x=450, y=300)
 
 
 
