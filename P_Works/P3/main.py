012eb1e46bf65d60ef005c974169f89d2968ce7b
lmaoaoaoao <jafar.radzhaboff@yandex.ru>
2025-11-30 20:05:29 +0300

    обновил показ поля

--- P_Works/P3/main.py
+++ P_Works/P3/main.py
@@ -1,7 +1,8 @@
 board = []
 
 
-
+''' Creating board '''
+#the size
 print ('Enter the board size:', end = '')
 while True:
     size = int(input())
@@ -10,26 +11,35 @@ while True:
     else:
         print ('Invalid size. Tru again -', end = '')
 
-
+#line by line
 line = []
 for i in range (size):
     line.append('•')
     i += 1
-    
+
 for i in range (size):
     board.append(line)
     i += 1
+''''''
 
 
 
 
 
 
-i = 0
-while i < size:    
-    j = 0
-    while j < size:
-        print (board[i][j], end = ' ')
-        j += 1
-    print ('')
-    i += 1
+
+
+#board show
+def b_show():
+    print ('  ', end = '')
+    for k in range (size):
+        print (k + 1, end = ' ')
+    print('')
+
+    for i in range (size):
+        print (i + 1, end = ' ')
+        for j in range (size):
+            print (board[i][j], end = ' ')
+            j += 1
+        print ('')
+        i += 1
