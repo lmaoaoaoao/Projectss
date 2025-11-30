75704669102849acd4ff80a3567c09a957658e33
lmaoaoaoao <jafar.radzhaboff@yandex.ru>
2025-11-30 19:25:50 +0300

    вывод поля

--- P_Works/P3/main.py
+++ P_Works/P3/main.py
@@ -1,3 +1,10 @@
+board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+
+size = 3
+
+
+
+'''
 print ('Enter the board size:', end = '')
 while True:
     size = int(input())
@@ -5,3 +12,23 @@ while True:
         break
     else:
         print ('Invalid size. Tru again -', end = '')
+
+
+i = 0
+while i < size:
+    board.append('•')
+    i += 1
+'''
+
+# print (board[1][1])
+
+
+
+i = 0
+while i < size:    
+    j = 0
+    while j < size:
+        print (board[i][j], end = ' ')
+        j += 1
+    print ('')
+    i += 1
